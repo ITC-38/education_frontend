f6b14729f65a2517efd4d1455226fd6de5f781e2
SaD-Pr0gEr <sad.pr0ger@gmail.com>
2023-07-31 17:39:17 +0500

    feature/course-detail-half-tmp

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,13 +1,14 @@
-let filters = document.querySelectorAll('.all-courses-filter > div .filter-header')
-filters.forEach((element) => {
-    element.addEventListener('click', () => {
-        let parentClass = element.parentElement.classList[0]
-        let filterBody = document.querySelector(
-            '.' + parentClass + ' .' + element.classList[0] + '+ .filter-body'
-        )
-        filterBody.classList.toggle('active')
-        element.querySelectorAll('p').forEach((el) => {
-            el.classList.toggle('active')
+function dropdown(elementsList, childClassName) {
+    elementsList.forEach((element) => {
+        element.addEventListener('click', () => {
+            let parentClass = element.parentElement.classList[0]
+            let filterBody = document.querySelector(
+                '.' + parentClass + ' .' + element.classList[0] + '+ .' + childClassName
+            )
+            filterBody.classList.toggle('active')
+            element.querySelectorAll('p').forEach((el) => {
+                el.classList.toggle('active')
+            })
         })
     })
-})
+}
